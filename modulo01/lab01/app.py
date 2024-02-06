8821805227454dbeb76a51deb04f7d49d88643dc
Adrian Candelaria <csjbacandelariagonzalez@gmail.com>
2024-02-06 14:18:06 -0400

    updatemyfiles

--- modulo01/lab01/app.py
+++ modulo01/lab01/app.py
@@ -1,3 +1,7 @@
 print('Hola Mundo!')
-mensaje = 'John Doe '
-print('mensaje')
+mensaje = 'John Doe '    # String value
+gpa = 3.14               # Number (real) value
+isValid = True           # Boolean value
+print(mensaje)
+print(gpa)
+print(isValid)
