9ca01013fae931203a09dec6eb3b1e617b8469a4
Adrian Candelaria <csjbacandelariagonzalez@gmail.com>
2024-02-27 13:55:11 -0400

    update files

--- modulo02/funcion04/app.py
+++ modulo02/funcion04/app.py
@@ -1,12 +1,12 @@
 def saludo():
     global nombre
-    global nombre = "John Doe"
+    nombre = "John Doe"
     print(f'hola {nombre}')
 
-    def despedida():
-        global nombre
-        nombre = "Sara Swan"
-        print(f'Adios {nombre}')
+def despedida():
+    global nombre
+    nombre = "Sara Swan"
+    print(f'Adios {nombre}')
 
 saludo()
 despedida()
