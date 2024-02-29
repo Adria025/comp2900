563f72d2f9fc0594b0b6b2de81bb8db9bca5e21c
Adrian Candelaria <csjbacandelariagonzalez@gmail.com>
2024-02-29 14:25:29 -0400

    update files

--- modulo02/arrays01/app.py
+++ modulo02/arrays01/app.py
@@ -7,4 +7,10 @@ for i in range(1, 4): #from 1 to 4
     aNumber.append(number)
 
 for i in range(3): #from 0 to 4
-    print(f'{i} - {aNumber[i]}')
+    print(f'Indice {i} - Valor {aNumber[i]}')
+
+for n in aNumber:
+    print(f'Valor {n}')
+
+for i in range(len(aNumber)): #from 0 to 2
+    print(f'Indice {i} - Valor {aNumber[i]}')
